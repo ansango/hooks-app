fd14bbfaf177cca123e5ca24b4e4e179bf4e664b
ansango <anibalsantosgo@gmail.com>
2021-08-17 20:41:48 +0200

    unmount

--- src/components/effect/Message.js
+++ src/components/effect/Message.js
@@ -1,11 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Message = () => {
-    useEffect(() => {
-        console.log("componente montado");
-        return () => {
-            console.log("componente desmontado");
-        }
-    }, [])
-  return <div>Message</div>;
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const { x, y } = coords;
+  useEffect(() => {
+    const mouseMove = (e) => setCoords({ x: e.x, y: e.y });
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+  return (
+    <div>
+      X: <p>{x}</p>
+      Y: <p>{y}</p>
+    </div>
+  );
 };
