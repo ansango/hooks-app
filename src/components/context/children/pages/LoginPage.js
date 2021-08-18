6c22845765996fd793dbd96704fd5bc7c248b07d
ansango <anibalsantosgo@gmail.com>
2021-08-18 20:24:16 +0200

    refactor

--- src/components/context/children/pages/LoginPage.js
+++ src/components/context/children/pages/LoginPage.js
@@ -1,17 +1,21 @@
 import { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import { log } from "../../../../helpers/log";
 import { Container } from "../Container";
 import { UserContext } from "../UserContext";
 
 export const LoginPage = () => {
+  const { push: goTo } = useHistory();
   const { user, setUser } = useContext(UserContext);
   log("MainApp - LoginPage", { user, setUser });
+
+  const handleClick = () => {
+    setUser({ id: 123, name: "Anibal" });
+    goTo("/");
+  };
   return (
     <Container title="Login">
-      <button
-        className="btn btn-primary"
-        onClick={() => setUser({ id: 123, name: "Anibal" })}
-      >
+      <button className="btn btn-primary" onClick={handleClick}>
         Login
       </button>
     </Container>
--- src/components/context/children/pages/LogoutPage.js
+++ src/components/context/children/pages/LogoutPage.js
@@ -6,10 +6,12 @@ import { UserContext } from "../UserContext";
 export const LogoutPage = () => {
   const { user, setUser } = useContext(UserContext);
   log("MainApp - LogoutPage", { user, setUser });
+
+  const handleClick = () => setUser({});
   return (
     <Container title="Logout">
       <pre>{JSON.stringify(user, null, 3)}</pre>
-      <button className="btn btn-warning" onClick={() => setUser({})}>
+      <button className="btn btn-warning" onClick={handleClick}>
         Logout
       </button>
     </Container>
