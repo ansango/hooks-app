dfe1a6a6ee287bdecdc34fa924f8970ad01753a1
ansango <anibalsantosgo@gmail.com>
2021-08-18 12:22:43 +0200

    layout

--- src/components/layoutEffect/LayoutEffect.js
+++ src/components/layoutEffect/LayoutEffect.js
@@ -1,3 +1,4 @@
+import { useLayoutEffect, useRef, useState } from "react";
 import { useCounter } from "../../hooks/useCounterCopy";
 import { useFetch } from "../../hooks/useFetch";
 
@@ -5,14 +6,24 @@ export const LayoutEffect = () => {
   const { state: counter, increment } = useCounter(1);
   const url = `https://www.breakingbadapi.com/api/quotes/${counter}`;
   const { data } = useFetch(url);
-  const { author, quote } = !!data && data[0];
+  const { quote } = !!data && data[0];
+
+  const pTag = useRef();
+  const [boxSize, setBoxSize] = useState({});
+  useLayoutEffect(() => {
+    const width = `${pTag.current.getBoundingClientRect().width} px`;
+    const height = `${pTag.current.getBoundingClientRect().height} px`;
+    setBoxSize({ width: width, height: height });
+  }, [quote]);
   return (
     <>
-      <blockquote className="blockquote text-end">
-        <p className="mb-3">{quote}</p>
-        <footer className="blockquote-footer">{author}</footer>
+      <blockquote className="blockquote d-flex">
+        <p className="mb-3" ref={pTag}>
+          {quote}
+        </p>
       </blockquote>
-
+      <p>Blockquote box dimensions:</p>
+      <pre>{JSON.stringify(boxSize, null, 3)}</pre>
       <button className="btn btn-primary" onClick={increment}>
         Next Quote
       </button>
