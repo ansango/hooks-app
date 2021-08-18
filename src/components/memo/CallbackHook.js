87b6a6481cab094409e6b76081d1fe1df08e8064
ansango <anibalsantosgo@gmail.com>
2021-08-18 13:09:02 +0200

    eff

--- src/components/memo/CallbackHook.js
+++ src/components/memo/CallbackHook.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ShowIncrement } from "./ShowIncrement";
 
 export const CallbackHook = () => {
@@ -10,6 +10,8 @@ export const CallbackHook = () => {
     },
     [setCounter]
   );
+
+  useEffect(() => {}, [increment]);
 
   return (
     <>
