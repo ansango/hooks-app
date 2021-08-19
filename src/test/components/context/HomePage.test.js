77e2f42cacdbd45f6c3e393565465b9bfe223d41
ansango <anibalsantosgo@gmail.com>
2021-08-19 16:04:27 +0200

    context test

---
+++ src/test/components/context/HomePage.test.js
@@ -0,0 +1,19 @@
+import { mount, shallow } from "enzyme";
+import { HomePage } from "../../../components/context/children/pages/HomePage";
+import { UserContext } from "../../../components/context/children/UserContext";
+
+describe("Test in <HomePage/>", () => {
+  const user = {
+    name: "Anibal",
+    email: "anibalsantosgo@gmail.com",
+  };
+  const wrapper = mount(
+    <UserContext.Provider value={{ user }}>
+      <HomePage />
+    </UserContext.Provider>
+  );
+
+  test("should render", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
